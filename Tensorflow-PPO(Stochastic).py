efede9307bf281ef134ebfd77adc97f1bee3bc74
MakovChen <98240703+MakovChen@users.noreply.github.com>
2023-04-02 15:58:16 +0800

    Update Tensorflow-PPO(Stochastic).py

--- Tensorflow-PPO(Stochastic).py
+++ Tensorflow-PPO(Stochastic).py
@@ -1,8 +1,6 @@
 """
 Within 5k episodes, 2/4 attempts solved LunarLanderContinous-v2 (achieved 200 moving averaged rewards).
 
-Some findings are:
-
 It seems better to stop clearing the replay buffer at every episode
 It is slow to learn! (perhaps due to some parameters settings...)
 Learning rate is important - as we all know :-) (these results haven't fully optimized it yet)
@@ -39,8 +37,7 @@ class ExperienceReplay():
     
     【method】
     1. __init__: create a memory when this object has been build.
-    2. get_experiments: Return the cumulative experience of state, reward, action, dist, next_state and done sequentially.
-    3. addExperience: Add a sample of state, reward, action, dist, next_state and done to __experience. 
+    2. addExperience: Add a sample of state, reward, action, dist, next_state and done to __experience. 
        //* definition of variables
        - state: list, [S1, S2...,Sm] (Information about the current environment)
        - reward: float, [R] (The points obtained for the actor's actions in the current state)
@@ -48,15 +45,12 @@ class ExperienceReplay():
        - dist: dictionary {'mu': [A1, A2...], 'sigma': [A1, A2...]}, (Action's measurement of stochastic distribution,contain mu and sigma.)
        - next_state: list, [S1, S2...,Sm] (New information about the environment after transition)
        - done: boolean (Showing the episode finished or not.)
-    4. clearExperience: Remove all experiences in __experience.
+    3. clearExperience: Remove all experiences in __experience.
   
     """
     def __init__(self):
         self.__experience = {'state':[], 'action':[],'dist':[], 'reward':[], 'next_state':[],'done':[]}
 
-    def get_experiments(self):
-        return self.__experience['state'], self.__experience['reward'], self.__experience['action'], self.__experience['dist'], self.__experience['next_state'], self.__experience['done']
-        
     def addExperience(self, state = None, reward = None, action = None, dist = None, next_state = None, done = None):
         self.__experience['state'].append(state)
         self.__experience['reward'].append(reward)
@@ -195,6 +189,21 @@ class Critic:
         return state_value
     
 class Actor_Critic_Method:
+    """
+    // Used to build Actor-Critic training framework.
+
+    【method】
+    1. __init__: Create a tensor diagram, and create Actor and Critic
+    2. get_action: Perform forward propagation on Actors to obtain action and their measurements
+    3. evaluate: Gradient descent on Critic and Actor in order
+    //* definition of variables
+       - state: 2D-list, [[S1, S2...,Sm],...] 
+       - action: 2D-list, [[A1, A2...,An],...] 
+       - reward: 1D-list, [[R],...] 
+       - dist: 1D-list, [{'mu': [A1, A2...], 'sigma': [A1, A2...]},...]
+       - epochs: int, (for descenting gradient many times with same batch to improve robustness)
+    
+    """
     def __init__(self, session, action_space, FLAGS):
         self.FLAGS = FLAGS
         self.session, self.actor, self.critic = session, Actor(action_space, FLAGS), Critic(FLAGS)
@@ -212,7 +221,7 @@ class Actor_Critic_Method:
         for _ in range(int(epochs * self.FLAGS.importance_sampling_k / self.FLAGS.batch_size )):
             idx = np.random.choice(len(state), size = self.FLAGS.batch_size, replace=False)
             batch_state, batch_reward = np.array([state[i] for i in idx]), np.array([reward[i] for i in idx])
-            _, loss = self.session.run([self.critic.train_op, self.critic.loss], { self.critic.state: batch_state , self.critic.td_target: batch_reward})
+            self.session.run(self.critic.train_op, { self.critic.state: batch_state , self.critic.td_target: batch_reward})
 
         for _ in range(int(epochs * self.FLAGS.importance_sampling_k / self.FLAGS.batch_size )):
             idx = np.random.choice(len(state), size = self.FLAGS.batch_size, replace=False)
@@ -221,59 +230,9 @@ class Actor_Critic_Method:
             advantage = np.array([batch_reward - state_value]).T
             batch_mu = np.array([dist[i]['mu'] for i in idx])
             batch_sigma = np.array([dist[i]['sigma'] for i in idx])
-            _, loss = self.session.run([self.actor.train_op, self.actor.loss], {self.actor.state: batch_state, self.actor.old_action: batch_action, self.actor.old_mu: batch_mu, self.actor.old_sigma: batch_sigma, self.actor.advantage: advantage})
+            self.session.run(self.actor.train_op, {self.actor.state: batch_state, self.actor.old_action: batch_action, self.actor.old_mu: batch_mu, self.actor.old_sigma: batch_sigma, self.actor.advantage: advantage})
 
-class Proximal_Policy_Optimization:
-    def __init__(self, env, FLAGS):
-        self.epochs = FLAGS.epochs
-        self.FLAGS = FLAGS
-        self.env = env
-        self.exp_replay = ExperienceReplay()
-        self.session = tf.InteractiveSession()
-        action_space = {'low': env.action_space.low[0], 'high': env.action_space.high[0]}
-        self.policy = Actor_Critic_Method(self.session, action_space, FLAGS)
-        self.session.run(tf.global_variables_initializer())
-        
-    def save_model(self):
-        saver = tf.train.Saver().save(self.session, "./model.ckpt")
-        
-    def update_policy(self, experiments):
-        # Monte Carlo Estimation
-        Monte_Carlo_reward, discounted_reward, flag = [], 0, False
-        for reward, done in zip(reversed(experiments['reward']), reversed(experiments['done'])):
-            if done:
-                flag = True
-                discounted_reward = 0
-            discounted_reward = reward + (self.FLAGS.gamma * discounted_reward)
-            Monte_Carlo_reward.insert(0, discounted_reward)
-        reward, state, action, dist = Monte_Carlo_reward, experiments['state'], experiments['action'], experiments['dist']
-        
-        #Update Actor-Critics Networks
-        self.policy.evaluate(state, action, reward, dist, self.FLAGS.epochs)
-        self.exp_replay.clear()
-
-    def playEpisode(self):
-        n, iters = 0, 0
-        while True:
-            state, done, total_reward = self.env.reset(), False, 0
-            while done == False:
-                self.env.render()
-                dist, action, value = self.policy.get_action(state)
-                action = action[0]
-                next_state, reward, done, _ = self.env.step(action)
-                if done == True and reward == -100:
-                    reward = 0
-                self.exp_replay.addExperience(state = state, reward = reward, action = action, dist = dist, next_state = next_state, done = done)
-                total_reward, state = total_reward + reward, next_state
-                iters += 1
-            print("episodes: %i, MA_total_reward: %.2f" %(n, total_reward))
-            if iters > self.FLAGS.importance_sampling_k:
-                self.update_policy(self.exp_replay.get_experiments())
-                iters = 0
-            n += 1
-            
-env = gym.make('LunarLanderContinuous-v2')
-
+#setup hyperparameters
 tf.app.flags.DEFINE_float('action_size', env.observation_space.shape[0], '')
 tf.app.flags.DEFINE_float('state_size', len(env.observation_space.sample()) , '')
 tf.app.flags.DEFINE_float('learning_rate_Actor', 2e-5, 'Learning rate for the policy estimator')
@@ -284,6 +243,39 @@ tf.app.flags.DEFINE_float('epsilon', 0.2, 'clipping surrogate interval')
 tf.app.flags.DEFINE_float('epochs', 128, 'PPO allow descenting policy gradient many times with same batch to improve policy network robustly')
 tf.app.flags.DEFINE_float('importance_sampling_k', 4000, 'for Monte Carlo heuristic approximation(According CLT, k can setting by everage step to approach target * 30)')
 
-if __name__ == "__main__":
-    PPO = Proximal_Policy_Optimization(env, tf.app.flags.FLAGS)
-    PPO.playEpisode()
+env = gym.make('LunarLanderContinuous-v2')
+exp_replay = ExperienceReplay()
+
+session = tf.InteractiveSession()
+A2C = Actor_Critic_Method(session,  {'low': env.action_space.low[0], 'high': env.action_space.high[0]}, FLAGS)
+session.run(tf.global_variables_initializer())
+
+n, iters = 0, 0
+While True:
+    state, done, total_reward = env.reset(), False, 0
+    #play episode
+    while done == False:
+        env.render()
+        dist, action, value = A2C.get_action(state)
+        next_state, reward, done, _ = env.step(action[0])
+        exp_replay.addExperience(state = state, reward = reward, action = action[0], dist = dist, next_state = next_state, done = done)
+        total_reward, state = total_reward + reward, next_state
+        iters += 1
+    print("episodes: %i, MA_total_reward: %.2f" %(n, total_reward))
+    
+    #training
+    if iters > self.FLAGS.importance_sampling_k:
+        # Monte Carlo Estimation
+        Monte_Carlo_reward, discounted_reward, flag = [], 0, False
+        for reward, done in zip(reversed(exp_replay['reward']), reversed(exp_replay['done'])):
+            if done:
+                flag = True
+                discounted_reward = 0
+            discounted_reward = reward + (self.FLAGS.gamma * discounted_reward)
+            Monte_Carlo_reward.insert(0, discounted_reward)
+            
+        #Update Actor-Critics Networks
+        A2C.evaluate(exp_replay['state'], exp_replay['action'], Monte_Carlo_reward, exp_replay['dist'], FLAGS.epochs)
+        exp_replay.clear()
+        iters = 0
+        n += 1       
